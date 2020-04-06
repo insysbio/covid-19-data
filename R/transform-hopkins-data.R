dbd9c75db8932f229d2f69b1c198e9a077bf2aa8
Evgeny Metelkin <evgeny.metelkin@gmail.com>
2020-04-06 19:40:02 +0300

    initial commit

---
+++ R/transform-hopkins-data.R
@@ -0,0 +1,116 @@
+### read and transform covid-19 data
+# https://github.com/CSSEGISandData/COVID-19
+
+# library('tidyr')
+# library('dplyr')
+# library('jsonlite')
+
+if(!require(tidyr, warn.conflicts = FALSE, lib.loc = 'r_libs')){
+    install.packages("tidyr", repos = 'https://cran.rstudio.com/', lib = 'r_libs')
+    library(tidyr, warn.conflicts = FALSE, lib.loc = 'r_libs')
+}
+if(!require(dplyr, warn.conflicts = FALSE, lib.loc = 'r_libs')){
+    install.packages("dplyr", repos = 'https://cran.rstudio.com/', lib = 'r_libs')
+    library(dplyr, warn.conflicts = FALSE, lib.loc = 'r_libs')
+}
+if(!require(jsonlite, warn.conflicts = FALSE, lib.loc = 'r_libs')){
+    install.packages("jsonlite", repos = 'https://cran.rstudio.com/', lib = 'r_libs')
+    library(jsonlite, warn.conflicts = FALSE, lib.loc = 'r_libs')
+}
+
+data.repos <- './input/hopkins'
+# setwd('..')
+
+output.path <- file.path(getwd(),'./output')
+
+# confirmed cases
+filename_confirmed <- file.path(data.repos, 'csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
+data_confirmed0 <- read.csv(filename_confirmed, stringsAsFactors = FALSE)
+date_confirmed_columns <- names(data_confirmed0)[-c(1,2,3,4)]
+data_confirmed0$data_type <- 'confirmed'
+
+# recovered cases
+filename_recovered <- file.path(data.repos, 'csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv')
+data_recovered0 <- read.csv(filename_recovered, stringsAsFactors = FALSE)
+date_recovered_columns <- names(data_recovered0)[-c(1,2,3,4)]
+data_recovered0$data_type <- 'recovered'
+
+# deaths cases
+filename_deaths <- file.path(data.repos, 'csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv')
+data_deaths0 <- read.csv(filename_deaths, stringsAsFactors = FALSE)
+date_deaths_columns <- names(data_deaths0)[-c(1,2,3,4)]
+data_deaths0$data_type <- 'deaths'
+
+# comparing tables
+diff_confirmed_recovered <- date_confirmed_columns != date_recovered_columns
+if (any(diff_confirmed_recovered)) {
+  stop('Inconsistency in columns between "confirmed" and "recovered"')
+}
+diff_confirmed_deaths <- date_confirmed_columns != date_deaths_columns
+if (any(diff_confirmed_deaths)) {
+  stop('Inconsistency in columns between "confirmed" and "deaths"')
+}
+
+# combining data
+
+combined_data <- bind_rows(data_confirmed0, data_recovered0, data_deaths0) %>% 
+  pivot_longer(date_confirmed_columns) %>%
+  pivot_wider(names_from = 'data_type', values_from = value) %>%
+  mutate(date = as.Date(name, "X%m.%d.%y")) %>%
+  mutate(area = paste(Country.Region, Province.State, sep = '_')) %>%
+  group_by(area) %>%
+  arrange(date, .by_group = TRUE) %>%
+  mutate(confirmed_new = confirmed - lag(confirmed, default = 0)) %>%
+  mutate(recovered_new = recovered - lag(recovered, default = 0)) %>%
+  mutate(deaths_new = deaths - lag(deaths, default = 0)) %>%
+  mutate(hasErrors = is.na(confirmed_new) || (confirmed_new < 0) || is.na(recovered_new) || (recovered_new < 0) || is.na(deaths_new) || (deaths_new < 0)) %>% #  || 
+  select(-name) %>%
+  ungroup()
+
+splitted_data <- combined_data %>%
+  split(f = combined_data$area) %>%
+  lapply(function(l){
+    list(
+      hasErrors = any(l$hasErrors),
+      Province.State = l$Province.State[1],
+      Country.Region = l$Country.Region[1],
+      Lat = l$Lat[1],
+      Long = l$Long[1],
+      area = l$area[1],
+      timeseries = l %>% select(date, confirmed, recovered, deaths, confirmed_new, recovered_new, deaths_new, hasErrors)
+    )
+  })
+
+### SAVE ALL TO FILE
+dir.create(output.path, showWarnings = FALSE)
+dir.create(file.path(output.path, 'hopkins'), showWarnings = FALSE)
+dir.create(file.path(output.path, 'hopkins', 'csv'), showWarnings = FALSE)
+dir.create(file.path(output.path, 'hopkins', 'json'), showWarnings = FALSE)
+
+# CSV all
+write.csv(combined_data, file.path(output.path, 'hopkins', 'csv', '_combined.csv') )
+# JSON all
+jsonlite::write_json(splitted_data, file.path(output.path, 'hopkins', 'json', '_combined.json'), pretty = TRUE, auto_unbox = TRUE)
+
+# CSV country
+combined_data$filename <- gsub('\\*', '_', combined_data$area)
+res_csv <- combined_data %>%
+  group_by(filename) %>%
+  group_walk(
+    ~ write.csv( .x, file.path(output.path, 'hopkins', 'csv', paste0(.y$filename,'.csv')) )
+  )
+# JSON country
+res_json <- splitted_data %>%
+  lapply(function (l) {
+    filename <- gsub('\\*', '_', l$area)
+    jsonlite::write_json(l, file.path(output.path, 'hopkins', 'json', paste0(filename,'.json')), pretty = TRUE, auto_unbox = TRUE)
+  })
+
+### PLOT
+
+#russia_data <- combined_data %>%
+#  subset(subset = area == 'Russia_')
+
+#plot(russia_data$date, russia_data$confirmed_new, log = 'y', xlab = 'Date', ylab = 'Confirmed new')
+#points(russia_data$date, russia_data$recovered_new, col='green')
+#points(russia_data$date, russia_data$deaths_new, col='red')
--- README.md
+++ README.md
@@ -1,2 +1,164 @@
-# covid-19-data
-Provide a simple and unified interface to COVID-19 latest datasets
+# COVID-19 data
+
+The goal of the project is to provide a simple and unified interface to COVID-19 latest datasets.
+
+The homepage is located here: <https://insysbio.github.io/covid-19-data/>
+
+## Table of contents
+
+- Usage
+    - Save as file
+    - R
+    - Julia
+    - Git
+- J.Hopkins' datasets
+    - JSON formatted
+    - CSV formatted
+    - Untransformed files
+- Contributing
+- Authors
+- License
+
+## Usage
+
+### Save as files
+
+Download combined CSV data as local file using bash shell
+
+```sh
+curl 'https://insysbio.github.io/covid-19-data/hopkins/csv/_combined.csv' --compressed > _combined.csv
+```
+
+### R
+
+### Julia
+
+### Git
+
+To clone the latest datasets to the directory *covid-19*
+
+```sh
+git clone -b docs --single-branch https://github.com/insysbio/covid-19-data.git covid-19
+```
+
+To update the previously cloned repository
+```sh
+cd covid-19
+git fetch
+git pull
+```
+
+## J.Hopkins' datasets
+
+The most popular COVID-19 dataset from J Hopkins team. The sources are located in [GitHub repository](https://github.com/CSSEGISandData/COVID-19) updated daily.
+
+The current interface performs some transformation and shares data from: 
+
+- *csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv*
+- *csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv*
+- *csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv*
+
+Files are combined and transformed to the following formats:
+
+### CSV formatted
+- https://insysbio.github.io/covid-19-data/hopkins/csv/_combined.csv
+- https://insysbio.github.io/covid-19-data/hopkins/csv/location-code.csv
+
+Available fields:
+
+|||
+--|--
+**Province.State** | Territory name from the original dataset
+**Country.Region** | Country name from the original dataset
+**Lat, Long** | Latitude and longitude from the original dataset
+**confirmed** | Confirmed cumulative cases
+**recovered** | Recovered cumulative cases
+**deaths** | Deaths cumulative cases
+**date** | Date in format YYYY-mm-dd
+**confirmed_new** | Confirmed cases for the date (calcuated as `today - yesterday`)
+**recovered_new** | Recovered cases for the date
+**deaths_new** | Deaths cases for the date
+**hasErrors** | If true there are missing data or inconsistency between yesterday and today
+
+### JSON formatted
+- https://insysbio.github.io/covid-19-data/hopkins/json/_combined.json
+- **https://insysbio.github.io/covid-19-data**/hopkins/json/location-code.json
+
+Available fields:
+|||
+--|--
+**Province.State** | Territory name from the original dataset
+**Country.Region** | Country name from the original dataset
+**Lat, Long** | Latitude and longitude from the original dataset
+**hasErrors** | If true there are errors in one of series data point
+**timeseries** | Array of time series data, see below
+
+Time series fields:
+|||
+--|--
+**date** | Date in format YYYY-mm-dd
+**confirmed** | Confirmed cumulative cases
+**recovered** | Recovered cumulative cases
+**deaths** | Deaths cumulative cases
+**confirmed_new** | Confirmed cases for the date (calcuated as `today - yesterday`)
+**recovered_new** | Recovered cases for the date
+**deaths_new** | Deaths cases for the date
+**hasErrors** | If true there are missig data or inconsistency between yesterday and today
+
+**Example**
+```json
+{
+  "Afghanistan_": {
+    "hasErrors": false,
+    "Province.State": "",
+    "Country.Region": "Afghanistan",
+    "Lat": 33,
+    "Long": 65,
+    "timeseries": [
+      {
+        "date": "2020-01-22",
+        "confirmed": 0,
+        "recovered": 0,
+        "deaths": 0,
+        "confirmed_new": 0,
+        "recovered_new": 0,
+        "deaths_new": 0,
+        "hasErrors": false
+      },
+      {
+        "date": "2020-01-23",
+        "confirmed": 0,
+        "recovered": 0,
+        "deaths": 0,
+        "confirmed_new": 0,
+        "recovered_new": 0,
+        "deaths_new": 0,
+        "hasErrors": false
+      },
+      ...
+    ]
+  },
+  ...
+}
+```
+
+### Untransformed files
+- https://insysbio.github.io/covid-19-data/hopkins/source/time_series_covid19_confirmed_global.csv
+- https://insysbio.github.io/covid-19-data/hopkins/source/time_series_covid19_recovered_global.csv
+- https://insysbio.github.io/covid-19-data/hopkins/source/time_series_covid19_deaths_global.csv*
+
+## Contributing
+
+Use [issues](https://github.com/insysbio/covid-19-data) page to write about your ideas and found bugs.
+
+
+## Authors
+
+- [Evgeny Metelkin](https://github.com/metelkin) 
+- [Aleksandr Stepanov](https://github.com/step-by-step)
+
+## License
+
+This repository is distributed under [MIT license](LICENSE.md).
+
+&copy; InSysBio LLC, 2020
