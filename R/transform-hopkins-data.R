404dfc84e5e0ef6c763b00b0446ebbeeda4c493a
metelkin <evgeny.metelkin@gmail.com>
2020-04-07 12:14:26 +0300

    remove installation to run

--- R/transform-hopkins-data.R
+++ R/transform-hopkins-data.R
@@ -5,19 +5,6 @@
 # library('dplyr')
 # library('jsonlite')
 
-if(!require(tidyr, warn.conflicts = FALSE)){
-    install.packages("tidyr", repos = 'https://cran.rstudio.com/', lib = 'r_libs')
-    library(tidyr, warn.conflicts = FALSE, lib.loc = 'r_libs')
-}
-if(!require(dplyr, warn.conflicts = FALSE)){
-    install.packages("dplyr", repos = 'https://cran.rstudio.com/', lib = 'r_libs')
-    library(dplyr, warn.conflicts = FALSE, lib.loc = 'r_libs')
-}
-if(!require(jsonlite, warn.conflicts = FALSE)){
-    install.packages("jsonlite", repos = 'https://cran.rstudio.com/', lib = 'r_libs')
-    library(jsonlite, warn.conflicts = FALSE, lib.loc = 'r_libs')
-}
-
 data.repos <- './input/hopkins'
 # setwd('..')
 
