92531b315c59ceaba77e1ffb4288a3f4a936e825
Evgeny Metelkin <evgeny.metelkin@gmail.com>
2020-04-09 20:29:29 +0300

    test

--- R/start.R
+++ R/start.R
@@ -22,7 +22,10 @@ if(!require(knitr, warn.conflicts = FALSE)){
     install.packages("knitr", repos = 'https://cran.rstudio.com/', lib = 'r_libs')
     library(knitr, warn.conflicts = FALSE, lib.loc = 'r_libs')
 }
-library(highr)
+if(!require(highr, warn.conflicts = FALSE)){
+    install.packages("highr", repos = 'https://cran.rstudio.com/', lib = 'r_libs')
+    library(highr, warn.conflicts = FALSE, lib.loc = 'r_libs')
+}
 
 res <- try({
   source('./R/transform-hopkins-data.R')
